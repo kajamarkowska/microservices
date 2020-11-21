3937794b2918095a7ab206f664fff5d1c9dfa70b
kaja.markowska <kaja.markowska@wp.pl>
2020-11-21 20:50:49 +0100

    Initial commit

---
+++ statistic/src/test/java/com/shop/statistic/StatisticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.statistic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatisticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
